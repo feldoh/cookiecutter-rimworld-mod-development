1187b74470b1ca91ca856aee5b1805a7c2ba9b89
Dexter Lowe <feldoh10@gmail.com>
2022-05-27 01:43:37 +0100

    Fix for path bug in no rider mode

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 #About is always made
 os.makedirs("{{cookiecutter.mod_ver}}/Defs", exist_ok=True)
@@ -6,3 +7,10 @@ os.makedirs("{{cookiecutter.mod_ver}}/Patches", exist_ok=True)
 os.makedirs("Common/Languages", exist_ok=True)
 os.makedirs("Common/Sounds", exist_ok=True)
 os.makedirs("Common/Textures", exist_ok=True)
+
+if os.path.exists("{{cookiecutter.mod_ver}}/Source/.run"):
+    os.makedirs("{{cookiecutter.mod_ver}}/Source/.idea", exist_ok=True)
+    shutil.move("{{cookiecutter.mod_ver}}/Source/.idea.{{cookiecutter.mod_name}}", "{{cookiecutter.mod_ver}}/Source/.idea")
+else:
+    shutil.rmtree("{{cookiecutter.mod_ver}}/Source/.norun", ignore_errors=False, onerror=None)
+    shutil.rmtree("{{cookiecutter.mod_ver}}/Source/.idea.{{cookiecutter.mod_name}}", ignore_errors=False, onerror=None)
